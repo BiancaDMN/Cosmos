e94b41102e52544e888d95234c6b1397bc56cc4b
BiancaDMN <73305260+BiancaDMN@users.noreply.github.com>
2022-10-05 10:41:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    $("#logo").lettering();
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+
+  showSlides(slideIndex += n);
+
+}
+
+function currentSlide(n) {
+
+  showSlides(slideIndex = n);
+
+}
+
+function showSlides(n) {
+
+  let i;
+  let slides = document.getElementsByClassName("package");
+
+  if (n > slides.length) {
+
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+
+    slides[i].style.display = "none";  
+
+  }
+
+  slides[slideIndex-1].style.display = "flex";  
+
+}
